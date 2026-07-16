98eafa90b41aef33a4ce0fbaba504aa0b5ca54d6
LuanaCvlcnt <lua.cavalcanti19@gmail.com>
2026-07-16 12:46:33 -0300

    adiciona logging com SLF4J nos testes de login

--- src/test/java/testes/LoginTest.java
+++ src/test/java/testes/LoginTest.java
@@ -1,6 +1,8 @@
 package testes;
 
 import java.time.Duration;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -10,7 +12,7 @@ import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
 public class LoginTest {
-	
+	private static final Logger logger = LoggerFactory.getLogger(LoginTest.class);
 	WebDriver driver;
 	
 	@BeforeClass
@@ -25,19 +27,22 @@ public class LoginTest {
 	@Test
 	void testLogin() 
 	{
-		
+		 logger.info("Iniciando teste de login");
+		 
 		LoginPageFactory lp=new LoginPageFactory(driver);
 		lp.setUserName("Admin");
 		lp.setPassword("admin123");
 		lp.clickLogin();
-				
-		AssertJUnit.assertEquals(driver.getTitle(),"OrangeHRM", driver.getTitle());
-		
+		logger.info("Login executado, validando título da página");		
+		AssertJUnit.assertEquals("OrangeHRM", driver.getTitle());;
+	    logger.info("Teste de login concluído com sucesso");
+
 	}
 	
 	@AfterClass
 	void tearDOwn()
 	{
+		logger.info("Encerrando WebDriver");
 		driver.quit();
 	}
 }
